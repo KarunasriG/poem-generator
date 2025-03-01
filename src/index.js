db2c93c99effe7a0da967a6253d89ea93d1d8ab6
KarunaSri Gaddam <gaddamkarunasri679@gmail.com>
2025-03-01 22:50:58 +0530

    enhanced the code & structure

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "7d8t09obdd36eff4c3d78d572abcad31";
     let context =
-      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 lines poem which are separated by each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
     let prompt = `User instructions: Generate a Telugu poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   
